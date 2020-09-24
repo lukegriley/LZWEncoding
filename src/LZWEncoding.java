0a846075c8bf01326416c9d099824a0b9b3e4ffd
Kosi Nwabueze <kosinwabueze@gmail.com>
2020-09-24 14:46:25 -0700

    chore: added user input to LZWEncoding

--- src/LZWEncoding.java
+++ src/LZWEncoding.java
@@ -1,44 +1,72 @@
 import java.io.*;
 import java.util.*;
+import java.nio.file.*;
 
 public class LZWEncoding {
 	public static void main(String[] args) throws IOException {
+		System.out.println("Enter a file path of the file you want to be encoded: ");
+		String inputFilename = null;
+
+		try (Scanner sc = new Scanner(System.in))
+		{
+			System.out.print("> ");			
+		
+			// Keep trying to prompt user input.
+			do {
+				inputFilename = sc.nextLine();
+
+				// Check if user input is an invalid file
+				if (Files.notExists(Paths.get(inputFilename))) {
+					System.out.println("Cannot find file.");
+					inputFilename = null;
+				}
+
+			} while (inputFilename == null);
+		}
+
 		LZWEncoding encoder = new LZWEncoding();
-		encoder.encode("lzw-text0.txt");
+
+		try {
+			encoder.encode(inputFilename);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		System.out.println("Encoded file at " + Paths.get(inputFilename + ".lzw"));
 	}
 
 	public void encode(String input) throws IOException {
 		File inputFile = new File(input);
 		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-		BufferedWriter writer = new BufferedWriter(new FileWriter(input.substring(0, input.length() - 4) + " encoded.txt"));
+		BufferedWriter writer = new BufferedWriter(
+				new FileWriter(new File(input + ".lzw")));
 
 		// create new file to write your encoded codestream and dictionary on
 		String previousChar = "";
 		String currentChar = "" + (char) reader.read();
 		String concatString = "" + currentChar;
 
-		// ArrayList<String> dictionary = new ArrayList<String>();
 		HashMap<String, Integer> encodingTable = new HashMap<String, Integer>();
 		HashMap<Integer, String> reverseTable = new HashMap<Integer, String>();
 
-		// maximum number of characters
-		final int maximum = 1 << 13;
+		// maximum number of entries in encoding table
+		final int MAXIMUM_ENRIES = 1 << 13;
 
-		for (int i = 0; i < 256; i++) // fill in the dictionary with the first known 255 characters and codes
+		for (int i = 0; i < 256; i++) // fill in the dictionary with the first known 256 characters and codes
 		{
 			String codepoint = "" + (char) i;
 			encodingTable.put(codepoint, i);
 		}
 
 		while (reader.ready()) {
-			// if it is already in the dictionary
+			// if concatString is already in the dictionary
 			if (encodingTable.containsKey(concatString)) {
 				previousChar = concatString;
-			} else // if it's not in the dictionary
+			} else // if concatString not in the dictionary
 			{
 				writer.write(encodingTable.get(previousChar) + ",");
 
-				if (encodingTable.get(previousChar) < maximum) {
+				if (encodingTable.get(previousChar) < MAXIMUM_ENRIES - 1) {
 					reverseTable.put(encodingTable.size(), concatString);
 					encodingTable.put(concatString, encodingTable.size());
 				}
@@ -46,15 +74,16 @@ public class LZWEncoding {
 				previousChar = currentChar;
 			}
 			currentChar = "" + (char) reader.read();
-			concatString = "" + previousChar + currentChar; // update c and concat
+			concatString = "" + previousChar + currentChar; // update currentChar and concatString
 		}
-		// end of while loop case - if the next P+C isn't in the dictionary, add it
+		// end of while loop case - if the next previous+current isn't in the dictionary, add it
 		if (!encodingTable.containsKey(concatString)) {
 			reverseTable.put(encodingTable.size(), "" + previousChar);
 			encodingTable.put("" + previousChar, encodingTable.size());
 		}
-		int lastIndex = encodingTable.get(concatString);// get the last index of the dictionary, write that to the file since
-													// the while loop ends one turn too early
+		int lastIndex = encodingTable.get(concatString);// get the last index of the dictionary, write that to the file
+														// since
+		// the while loop ends one turn too early
 
 		writer.write("" + lastIndex + ",");
 		writer.write(";");
