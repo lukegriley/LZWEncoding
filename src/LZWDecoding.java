9a8f0d7f03543e432cb6fdb512f766049fe6c64e
Kosi Nwabueze <kosinwabueze@gmail.com>
2020-09-28 17:19:46 -0700

    feat: optimized LZWDecoding.java and added better comments

--- src/LZWDecoding.java
+++ src/LZWDecoding.java
@@ -1,31 +1,41 @@
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
+import java.nio.file.*;
 
 public class LZWDecoding {
+    /**
+     * CLI tool for LZW decoder.
+     * 
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         System.out.println("Enter the path of the file you want to be decoded");
-        String inputFilename = null;
+        String inputFileName = null;
 
         try (Scanner sc = new Scanner(System.in)) {
-            // Keep trying to prompt user input.
+            /**
+             * Keep prompting the user until they input a valid file path.
+             */
             do {
                 System.out.print("> ");
-                inputFilename = sc.nextLine();
+
+                inputFileName = sc.nextLine();
 
                 // Check if user input is an invalid file
-                if (Files.notExists(Paths.get(inputFilename))) {
+                if (inputFileName.equals("") || Files.notExists(Paths.get(inputFileName))) {
                     System.out.println("Cannot find file.");
-                    inputFilename = null;
+                    inputFileName = null;
                 }
 
-            } while (inputFilename == null);
+            } while (inputFileName == null);
         }
 
         long startTime = System.nanoTime();
+
         LZWDecoding decoder = new LZWDecoding();
-        decoder.decode(inputFilename);
+        decoder.decode(inputFileName);
+
         long endTime = System.nanoTime();
 
         final double ns_to_ms = 1000000;
@@ -33,44 +43,110 @@ public class LZWDecoding {
         System.out.println("total runtime (ms): " + (double) (endTime - startTime) / ns_to_ms);
     }
 
+    /**
+     * Applies LZW decoding to given file. Places decoded file in same directory as
+     * input file without ".lzw" extension.
+     * 
+     * @param inputFileName Path of file to be decoded
+     * @throws IOException
+     */
     public void decode(String inputFileName) throws IOException {
-        String outputFileName = inputFileName.replaceAll("\\.lzw$", "");// get the original file name
+        String outputFileName = inputFileName.replaceAll("\\.lzw$", "");
 
-        try (FileReader inputReader = new FileReader(inputFileName)) {
+        try (BufferedReader inputReader = new BufferedReader(new FileReader(inputFileName))) {
+            /**
+             * decodingTable represents a mapping from a substitute index to original text.
+             * 
+             * Substitute values in encoded text will be replaced with original text as part
+             * of the LZW decoding algorithm.
+             * 
+             * This table is populated with the extended ASCII character set.
+             * 
+             * Example mappings:
+             * 
+             * 0x41 => "A" | 0x42 => "B" | 0x0A => "\n"
+             */
             HashMap<Integer, String> decodingTable = new HashMap<Integer, String>();
 
             for (int i = 0; i < 256; i++) {
                 decodingTable.put(i, "" + (char) i);
             }
 
+            /**
+             * Read entire file into a StringBuilder (faster than using Strings which are
+             * immutable). Whole file needs to be read first, since the end of the file is
+             * read before the beginning. StringBuilder is converted into String at the end
+             * for access to String methods.
+             */
             StringBuilder inputBuffer = new StringBuilder();
-
             int fileCursor;
 
             while ((fileCursor = inputReader.read()) != -1) {
                 inputBuffer.append((char) fileCursor);
             }
 
-            String[] splittext = inputBuffer.toString().split(";", 2);
+            String inputtext = inputBuffer.toString();
 
-            String legend = splittext[1];
-            String ciphertext = splittext[0];
+            /**
+             * Initialize variables used in the LZW decoding algorithm.
+             * 
+             * ciphertext: Encoded text of substitute values.
+             * 
+             * legend: Partially serialized form of encodingTable (see LZWEncoding.java).
+             */
 
-            while (legend.length() > 0) {
-                String[] tokens = legend.split("=", 2);
+            String ciphertext = inputBuffer.substring(0, inputtext.indexOf(';'));
+            String legend = inputBuffer.substring(ciphertext.length() + 1);
 
-                int nextLen = Integer.parseInt(tokens[0]);
-                String nextEntry = tokens[1].substring(0, nextLen);
+            /**
+             * Deserialize legend into decodingTable.
+             * 
+             * Example:
+             * 
+             * legend: 2=ab3=abc4=bd
+             * decodingTable: 256 => "ab" | 257 => "abc" | 258 => "bd"
+             */
+            int i = 0;
+            while (i < legend.length()) {
+                /**
+                 * Index of next '=' (delimeter) starting from index i.
+                 */
+                int nextEqualsIndex = legend.indexOf('=', i);
+
+                /**
+                 * Number of characters to read to form the next entry of the decoding table.
+                 */
+                String nextLen = legend.substring(i, nextEqualsIndex);
+
+                /**
+                 * Text of next entry in the decodingTable.
+                 */
+                String nextEntry = legend.substring(nextEqualsIndex + 1,
+                        nextEqualsIndex + 1 + Integer.parseInt(nextLen));
 
                 decodingTable.put(decodingTable.size(), nextEntry);
 
-                legend = tokens[1].substring(nextLen);
+                /**
+                 * Increment i to move past characters already read.
+                 */
+                i = nextEqualsIndex + 1 + nextEntry.length();
             }
 
-            try (FileWriter outputWriter = new FileWriter(outputFileName)) {
-                for (String token : ciphertext.split(",")) {
-                    int substitute = Integer.parseInt(token);
-                    outputWriter.write(decodingTable.get(substitute));
+            /**
+             * Decode ciphertext back into original plaintext.
+             * 
+             * Tokenizes ciphertext based on delimeter (',') and replaces each
+             * subsitute value with its original plaintext equivalent.
+             */
+            i = 0;
+            try (BufferedWriter outputWriter = new BufferedWriter(new FileWriter(outputFileName))) {
+                while (i < ciphertext.length()) {
+                    int nextCommaIndex = ciphertext.indexOf(',', i);
+
+                    String nextSubstitute = ciphertext.substring(i, nextCommaIndex);
+                    outputWriter.write(decodingTable.get(Integer.parseInt(nextSubstitute)));
+
+                    i = nextCommaIndex + 1;
                 }
             }
         }
--- src/LZWEncoding.java
+++ src/LZWEncoding.java
@@ -3,94 +3,167 @@ import java.util.*;
 import java.nio.file.*;
 
 public class LZWEncoding {
+	/**
+	 * CLI tool for LZW encoder.
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
 		System.out.println("Enter the path of the file you want to be encoded");
-		String inputFilename = null;
+		String inputFileName = null;
 
-		try (Scanner sc = new Scanner(System.in))
-		{		
-			// Keep trying to prompt user input.
+		try (Scanner sc = new Scanner(System.in)) {
+			/**
+			 * Keep prompting the user until they input a valid file path.
+			 */
 			do {
 				System.out.print("> ");
 
-				inputFilename = sc.nextLine();
+				inputFileName = sc.nextLine();
 
 				// Check if user input is an invalid file
-				if (inputFilename.equals("") || Files.notExists(Paths.get(inputFilename))) {
+				if (inputFileName.equals("") || Files.notExists(Paths.get(inputFileName))) {
 					System.out.println("Cannot find file.");
-					inputFilename = null;
+					inputFileName = null;
 				}
 
-			} while (inputFilename == null);
+			} while (inputFileName == null);
 		}
 
-        long startTime = System.nanoTime();
-        LZWEncoding encoder = new LZWEncoding();
-        encoder.encode(inputFilename);
-		long endTime = System.nanoTime();
-		
-		final double ns_to_ms = 1000000;
-        
-        System.out.println("total runtime (ms): " + (double)(endTime-startTime)/ns_to_ms);
+		long startTime = System.nanoTime(); // record start time
+
+		LZWEncoding encoder = new LZWEncoding();
+		encoder.encode(inputFileName);
+
+		long endTime = System.nanoTime(); // record end time
+
+		final double ns_to_ms = 1000000; // number of nanoseconds in a millisecond
+
+		System.out.println("total runtime (ms): " + (double) (endTime - startTime) / ns_to_ms);
 	}
 
-	public void encode(String input) throws IOException {
-		File inputFile = new File(input);
-		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-		BufferedWriter writer = new BufferedWriter(
-				new FileWriter(new File(input + ".lzw")));
+	/**
+	 * Applies LZW encoding to given file. Places encoded file in same 
+	 * directory as input file with extension ".lzw".
+	 * 
+	 * @param inputFileName Path of file to be encoded
+	 * @throws IOException
+	 */
+	public void encode(String inputFileName) throws IOException {
+		String outputFileName = inputFileName + ".lzw";
 
-		// create new file to write your encoded codestream and dictionary on
-		String previousChar = "";
-		String currentChar = "" + (char) reader.read();
-		String concatString = "" + currentChar;
+		try (BufferedReader inputReader = new BufferedReader(new FileReader(inputFileName))) {
 
-		HashMap<String, Integer> encodingTable = new HashMap<String, Integer>();
-		HashMap<Integer, String> reverseTable = new HashMap<Integer, String>();
+			/**
+			 * Initialize variables used in the LZW algorithm implementation.
+			 * 
+			 * previousString: represents the previously-read characters in the stream.
+			 * currentChar: represents the current character being read in the stream.
+			 * concatString: an intermediate value for previousString + currentChar
+			 */
+			String previousString = "";
+			String currentChar = "" + (char) inputReader.read();
+			String concatString = "" + currentChar;
 
-		// maximum number of entries in encoding table
-		final int MAXIMUM_ENRIES = 1 << 13;
+			/**
+			 * encodingTable represents a mapping from a string to a substitute value.
+			 * 
+			 * These substitute values will be placed into the output value as part of the
+			 * LZW encoding algorithm.
+			 * 
+			 * This table is first populated with the entirety of the extended ASCII codeset
+			 * (256 characters).
+			 * 
+			 * Example mappings:
+			 * 
+			 * "A" => 0x41 | "B" => 0x42 | "\n" => 0x0A
+			 */
+			HashMap<String, Integer> encodingTable = new HashMap<String, Integer>();
 
-		for (int i = 0; i < 256; i++) // fill in the dictionary with the first known 256 characters and codes
-		{
-			String codepoint = "" + (char) i;
-			encodingTable.put(codepoint, i);
-		}
+			/**
+			 * reverseTable represents a mapping from a substitute value to a string.
+			 * 
+			 * This table is used to place the values of the encodingTable in sequential
+			 * order after the ciphertext (encoded text) and makes the LZW decoding
+			 * algorithm much simpler.
+			 */
+			HashMap<Integer, String> reverseTable = new HashMap<Integer, String>();
 
-		while (reader.ready()) {
-			// if concatString is already in the dictionary
-			if (encodingTable.containsKey(concatString)) {
-				previousChar = concatString;
-			} else // if concatString not in the dictionary
-			{
-				writer.write(encodingTable.get(previousChar) + ",");
+			/**
+			 * Maximum number of entries in the encoding table.
+			 * 
+			 * Currently, the maximum entries value is fixed to a 13-bit limit.
+			 * 
+			 * 13-bit integers can only represent up to 8192 (the largest index is 8191).
+			 */
+			final int MAXIMUM_ENRIES = 1 << 13;
 
-				if (encodingTable.get(previousChar) < MAXIMUM_ENRIES - 1) {
-					reverseTable.put(encodingTable.size(), concatString);
-					encodingTable.put(concatString, encodingTable.size());
+			/**
+			 * Fill the encoding table with the extended ASCII character set.
+			 */
+			for (int i = 0; i < 256; i++) {
+				String codepoint = "" + (char) i;
+				encodingTable.put(codepoint, i);
+			}
+
+			try (BufferedWriter outputWriter = new BufferedWriter(new FileWriter(outputFileName))) {
+				/**
+				 * Algorithm description
+				 * 				 
+				 * - if concatString is in encodingTable
+				 * 	- previousString = concatString
+				 * - else
+				 * 	- output substitute for previousString into codestream
+				 * 	- put concatString into encodingTable
+				 * 	- previousString = currentChar
+				 * - currentChar = next character in input stream				 
+				 * - concatString = previousString + currentChar
+				 */
+				while (inputReader.ready()) {
+					// if concatString is already in the dictionary
+					if (encodingTable.containsKey(concatString)) {
+						previousString = concatString;
+					} else { // if concatString not in the dictionary					
+						outputWriter.write(encodingTable.get(previousString) + ",");
+
+						if (encodingTable.size() < MAXIMUM_ENRIES) {
+							reverseTable.put(encodingTable.size(), concatString);
+							encodingTable.put(concatString, encodingTable.size());
+						}
+
+						previousString = currentChar;
+					}
+					currentChar = "" + (char) inputReader.read();
+					concatString = "" + previousString + currentChar; // update currentChar and concatString
+				}
+				
+				/**
+				 * Handle edge case where the last few characters of the input stream have no substitute.
+				 */
+				if (!encodingTable.containsKey(concatString)) {
+					reverseTable.put(encodingTable.size(), "" + previousString);
+					encodingTable.put("" + previousString, encodingTable.size());
 				}
 
-				previousChar = currentChar;
+				/**
+				 * Output last substitute value into file.
+				 */
+				int lastSubstitute = encodingTable.get(concatString);				
+
+				outputWriter.write("" + lastSubstitute + ",");
+				outputWriter.write(";");
+
+				/**
+				 * Output encodingTable information into the output file.
+				 * 
+				 * Format (where substring is the original string put into the encodingTable):
+				 * `${substring.length()}=${substring}`
+				 */
+				for (int i = 256; i < encodingTable.size(); i++) {
+					outputWriter.write(reverseTable.get(i).length() + "=" + reverseTable.get(i));
+				}
 			}
-			currentChar = "" + (char) reader.read();
-			concatString = "" + previousChar + currentChar; // update currentChar and concatString
 		}
-		// end of while loop case - if the next previous+current isn't in the dictionary, add it
-		if (!encodingTable.containsKey(concatString)) {
-			reverseTable.put(encodingTable.size(), "" + previousChar);
-			encodingTable.put("" + previousChar, encodingTable.size());
-		}
-		int lastIndex = encodingTable.get(concatString);// get the last index of the dictionary, write that to the file
-														// since
-		// the while loop ends one turn too early
-
-		writer.write("" + lastIndex);
-		writer.write(";");
-
-		for (int i = 256; i < encodingTable.size(); i++) {// output all the unknown codes to the end of the encoded file
-			writer.write(reverseTable.get(i).length() + "=" + reverseTable.get(i));
-		}
-		writer.close();
-		reader.close();
 	}
 }
